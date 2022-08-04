1190db9716de60a2ecce422d7095f3814b57c898
cang <cangzx@gmail.com>
2022-08-03 21:45:12 -0400

    updated to v0.2

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='commot',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
     description='Cell-cell communications in space of transcriptomics data via collective optimal transport.',
     author='Zixuan Cang',
     author_email='cangzx@gmail.com'
