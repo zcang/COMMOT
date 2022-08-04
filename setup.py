ad73d443b45392a675d241568985914631cff891
cang <cangzx@gmail.com>
2022-08-04 12:13:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='commot',
     version='0.0.2',
@@ -16,4 +19,6 @@ setup(
     	"Operating System :: MacOS",
     	"Topic :: Scientific/Engineering",
     	"Topic :: Scientific/Engineering :: Bio-Informatics"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
