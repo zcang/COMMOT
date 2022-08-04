86e59cfb10f0cf32ca712b2c11073e22ca3eb4b0
cang <cangzx@gmail.com>
2022-08-04 12:02:50 -0400

    Update files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='commot',
-    version='0.2',
+    version='0.0.2',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     description='Cell-cell communications in space of transcriptomics data via collective optimal transport.',
